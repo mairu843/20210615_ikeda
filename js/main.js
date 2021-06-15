e6e759b54edfed36a265fde64bbc69c08c283f8c
まいる <masaya843@gmail.com>
2021-06-15 10:21:13 +0900

    20210615_ikeda

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// index JavaScript
+const target = document.getElementById("menu")
+target.addEventListener('click', () => {
+    const target = document.getElementById('menu');
+    target.classList.toggle('open');
+    const nav = document.getElementById('drawer');
+    nav.classList.toggle('in');
+})
+
+// service JavaScript
